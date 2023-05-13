01beb1143eccbfac082970ee65e9b1546bf8fb9e
VictorTamayoBriones <victortamayo509@gmail.com>
2023-05-12 23:40:27 -0600

    chore: :tada: init project
    theme, assets and initial config

---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+export const theme = {
+    card_bg: "#E9F1EE80",
+    yellow: "#FFE084",
+    green: "#196046",
+    green_light:"#469175",
+    white:"#FFFFFF",
+    grey_dark:"#303C51",
+    natural:"#62728F"
+}
