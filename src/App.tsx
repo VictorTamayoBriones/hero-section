64e27a7ce239b665330e66d3de90caa12475c35c
VictorTamayoBriones <victortamayo509@gmail.com>
2023-05-13 16:54:28 -0600

    feat: :sparkles: hero images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import Hero from "./components/Hero/Hero"
 import Footer from "./components/Hero/components/Footer/Footer"
 import MainInfo from "./components/Hero/components/MainInfo/MainInfo"
+import HeroImage from './assets/images/img-main.png';
+import Circles from "./assets/icons/Circles/Circles";
+import Square from "./assets/icons/Square/square";
 
 function App() {
   return (
@@ -8,6 +11,11 @@ function App() {
       <div className="left">
         <MainInfo/>
         <Footer/>
+      </div>
+      <div className="right">
+        <Circles/>
+        <img src={HeroImage} alt="hero-image" />
+        <Square/>
       </div>
     </Hero>
   )
--- src/components/Hero/hero.style.tsx
+++ src/components/Hero/hero.style.tsx
@@ -1,14 +1,47 @@
 import styled from 'styled-components';
 
+
 export const HeroContainer = styled.div`
     width: 100vw;
     height: 100vh;
     padding: 133px 150px;
+    display: flex;
+    justify-content: space-between;
 
     .left{
         height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
+    }
+
+    .right{
+        width: 50%;
+        height: 100%;
+        position: relative;
+
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+            position: relative;
+            z-index: 2;
+        }
+
+        svg{
+            &:nth-child(1){
+                position: absolute;
+                top: 40px;
+                left: 15px;
+                z-index: 3;
+            }
+
+            &:last-child{
+                position: absolute;
+                bottom: -24px;
+                right: 45px;
+            }
+
+        }
     }
 `;
